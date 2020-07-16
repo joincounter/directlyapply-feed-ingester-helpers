4af91962f9ed60d74f0274f2c1a445fc29fc0ae6
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2020-07-16 20:06:22 +0100

    minor change

--- existingJobsFilter.go
+++ existingJobsFilter.go
@@ -1,19 +1,30 @@
 package helpers
 
 // ExistingJobFilter a helper to remove existing jobs
-func ExistingJobFilter(jobs []StandardJob, existingJobs []PersistentIndexJob) ([]StandardJob, int) {
+func ExistingJobFilter(jobs []StandardJob, existingJobs []PersistentIndexJob) ([]StandardJob, []string, int) {
 	jobsRemovedCount := 0
 	jobsNotInDB := make([]StandardJob, 0)
+	existingJobsMap := make(map[PersistentIndexJob]bool)
+	for i := 0; i < len(existingJobs); i++ {
+		existingJobsMap[existingJobs[i]] = false
+	}
 UpperLoop:
 	for i := 0; i < len(jobs); i++ {
-		for j := 0; j < len(existingJobs); j++ {
-			url := existingJobs[j].URL
+		for job := range existingJobsMap {
+			url := job.URL
 			if jobs[i].URL == url {
 				jobsRemovedCount++
+				existingJobsMap[job] = true
 				break UpperLoop
 			}
 		}
 		jobsNotInDB = append(jobsNotInDB, jobs[i])
 	}
-	return jobsNotInDB, jobsRemovedCount
+	jobIDsNotInFeed := make([]string, 0)
+	for job, inFeed := range existingJobsMap {
+		if !inFeed {
+			jobIDsNotInFeed = append(jobIDsNotInFeed, job.ID)
+		}
+	}
+	return jobsNotInDB, jobIDsNotInFeed, jobsRemovedCount
 }
