bdcf04822faa484c8da097e2c1f18979a5af72f7
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2020-07-28 17:09:04 +0100

    added RemoteAvalible option to standardJob

--- standardJob.go
+++ standardJob.go
@@ -7,19 +7,20 @@ import (
 
 // StandardJob generic job to be used accross the system
 type StandardJob struct {
-	JobID       string    `json:"jobid"`
-	Title       string    `json:"title"`
-	URL         string    `json:"url"`
-	Company     string    `json:"company"`
-	Slug     	string    `json:"slug"`
-	City        string    `json:"city"`
-	CPA         float32   `json:"cpa,omitempty"`
-	CPC         float32   `json:"cpc,omitempty"`
-	Description string    `json:"description"`
-	Date        time.Time `json:"date"`
-	Country     string    `json:"country"`
-	ZIP     	string    `json:"zip"`
-	State     	string    `json:"state"`
+	JobID          string    `json:"jobid"`
+	Title          string    `json:"title"`
+	URL            string    `json:"url"`
+	Company        string    `json:"company"`
+	Slug           string    `json:"slug"`
+	City           string    `json:"city"`
+	CPA            float32   `json:"cpa,omitempty"`
+	CPC            float32   `json:"cpc,omitempty"`
+	Description    string    `json:"description"`
+	Date           time.Time `json:"date"`
+	Country        string    `json:"country"`
+	ZIP            string    `json:"zip"`
+	State          string    `json:"state"`
+	RemoteAvalible bool      `json:"remoteAvalible"`
 }
 
 // EstimatedValue standard calc for the jobs value
