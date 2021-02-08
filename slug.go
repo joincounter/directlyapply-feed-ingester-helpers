8dadc2215cc9585de015c5799c198d492304ca27
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-02-08 22:55:46 +0000

    add LocationSlug to helpers

--- slug.go
+++ slug.go
@@ -1,8 +1,21 @@
 package helpers
 
 import (
+	"fmt"
 	"strings"
 )
+
+// LocationSlug makes a unique slug from a region and a city
+func LocationSlug(region string, city string) string {
+	city = strings.ToLower(city)
+	city = strings.Replace(city, " ", "-", -1)
+	if region == "" {
+		return city
+	}
+	region = strings.ToLower(region)
+	region = strings.Replace(region, " ", "-", -1)
+	return fmt.Sprintf("%s//%s", region, city)
+}
 
 // GenerateSlug is a standard slug generating function
 func GenerateSlug(companyName string) string {
