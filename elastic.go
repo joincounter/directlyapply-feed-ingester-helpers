2387aa24edca503adddd8ffc1db0742115ae8676
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-03-10 13:19:04 +0000

    fix

--- elastic.go
+++ elastic.go
@@ -8,7 +8,7 @@ import (
 )
 
 // RemoveFromElastic remove list of IDs from elastic
-func RemoveFromElastic(jobIDs []string, hostname string, token string) (*http.Response, error) {
+func RemoveFromElastic(jobIDs []string, countryCode string, token string) (*http.Response, error) {
 	type deletionStruct struct {
 		Query struct {
 			Terms struct {
@@ -24,12 +24,17 @@ func RemoveFromElastic(jobIDs []string, hostname string, token string) (*http.Re
 		return nil, err
 	}
 	payload := strings.NewReader(string(jsonData))
-	url := fmt.Sprintf("https://%s/jobs_ca/_delete_by_query", hostname)
-	req, err := http.NewRequest("POST", url, payload)
+	req, err := http.NewRequest("POST", elasticURLs[countryCode], payload)
 	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add("authorization", fmt.Sprintf("Basic %s", token))
 	return http.DefaultClient.Do(req)
+}
+
+var elasticURLs = map[string]string{
+	"US": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs_us/_delete_by_query",
+	"UK": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs/_delete_by_query",
+	"CA": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs_ca/_delete_by_query",
 }
