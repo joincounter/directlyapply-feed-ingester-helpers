c145ce40a3be70c0a6adc82491929a8be39c9f0a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-05-04 16:15:31 +0100

    removed unused varibles

--- persistentIndex.go
+++ persistentIndex.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -53,13 +52,8 @@ func DeletePersistentJobs(client mongo.Client, ids []string, serverAddr string, 
 
 // PersistentIndexJob minimum representaion of a job
 type PersistentIndexJob struct {
-	ID         string    `bson:"_id" db:"external_id"`
-	URL        string    `bson:"url" db:"url"`
-	Title      string    `db:"title"`
-	CompanyID  uuid.UUID `db:"company_id"`
-	LocationID uuid.UUID `db:"location_id"`
-	CPA        float32   `db:"cpa"`
-	CPC        float32   `db:"cpc"`
+	ID  string `bson:"_id" db:"external_id"`
+	URL string `bson:"url" db:"url"`
 }
 
 var countryDatabases = map[string]string{
