4484f81ec6f9d82eabcb81227c349a9be1375b44
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-05-05 12:26:53 +0100

    added BatchStandardJobs

--- helpers.go
+++ helpers.go
@@ -1,5 +1,9 @@
 package helpers
 
+import (
+	"math"
+)
+
 // StringInSlice figures out if string is in slice
 func StringInSlice(str string, list []string) bool {
 	for _, v := range list {
@@ -8,4 +12,18 @@ func StringInSlice(str string, list []string) bool {
 		}
 	}
 	return false
+}
+
+func BatchStandardJobs(jobs []StandardJob, batchSize int) [][]StandardJob {
+	batchedJobs := make([][]StandardJob, 0)
+	var batchMarker int
+	for {
+		jobsInThisBatch := int(math.Min(float64(len(jobs)-batchMarker), float64(batchSize)))
+		batchedJobs = append(batchedJobs, jobs[batchMarker:batchMarker+jobsInThisBatch])
+		batchMarker += batchSize
+		if jobsInThisBatch < batchSize {
+			break
+		}
+	}
+	return batchedJobs
 }
