6fbc592675f24f756a97492d51c6aab851b94366
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-05-25 13:12:45 +0100

    try again

--- existingJobsFilter.go
+++ existingJobsFilter.go
@@ -2,29 +2,29 @@ package helpers
 
 // ExistingJobFilter a helper to remove existing jobs
 func ExistingJobFilter(jobs []StandardJob, existingJobs []PersistentIndexJob) ([]StandardJob, []string, int) {
-	jobsRemovedCount := 0
-	jobsNotInDB := make([]StandardJob, 0)
-	existingJobsMap := make(map[PersistentIndexJob]bool)
+	existingJobsURLMap := make(map[string]string)
 	for i := 0; i < len(existingJobs); i++ {
-		existingJobsMap[existingJobs[i]] = false
+		existingJobsURLMap[existingJobs[i].URL] = existingJobs[i].ID
 	}
-UpperLoop:
+	feedJobsURLMap := make(map[string]StandardJob)
 	for i := 0; i < len(jobs); i++ {
-		for job := range existingJobsMap {
-			url := job.URL
-			if jobs[i].URL == url {
-				jobsRemovedCount++
-				existingJobsMap[job] = true
-				break UpperLoop
-			}
-		}
-		jobsNotInDB = append(jobsNotInDB, jobs[i])
+		feedJobsURLMap[jobs[i].URL] = jobs[i]
 	}
+
+	jobsNotInDB := make([]StandardJob, 0)
+	for i := 0; i < len(jobs); i++ {
+		if _, inDatabase := existingJobsURLMap[jobs[i].URL]; !inDatabase {
+			jobsNotInDB = append(jobsNotInDB, jobs[i])
+		}
+	}
+
 	jobIDsNotInFeed := make([]string, 0)
-	for job, inFeed := range existingJobsMap {
-		if !inFeed {
-			jobIDsNotInFeed = append(jobIDsNotInFeed, job.ID)
+	for i := 0; i < len(existingJobs); i++ {
+		if _, inFeed := feedJobsURLMap[existingJobs[i].URL]; !inFeed {
+			jobIDsNotInFeed = append(jobIDsNotInFeed, existingJobs[i].ID)
 		}
 	}
+
+	jobsRemovedCount := len(jobs) - len(jobsNotInDB)
 	return jobsNotInDB, jobIDsNotInFeed, jobsRemovedCount
 }
