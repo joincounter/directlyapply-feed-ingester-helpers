b9b3ab8d85596f191ba3e35df0c8978fe7248572
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-06-09 17:32:47 +0100

    added PushToElastic

--- elastic.go
+++ elastic.go
@@ -24,7 +24,7 @@ func RemoveFromElastic(jobIDs []string, countryCode string, token string) (*http
 		return nil, err
 	}
 	payload := strings.NewReader(string(jsonData))
-	req, err := http.NewRequest("POST", elasticURLs[countryCode], payload)
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/%s/_delete_by_query", elasticDBs[countryCode]), payload)
 	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
 		return nil, err
@@ -33,8 +33,26 @@ func RemoveFromElastic(jobIDs []string, countryCode string, token string) (*http
 	return http.DefaultClient.Do(req)
 }
 
-var elasticURLs = map[string]string{
-	"US": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs_us/_delete_by_query",
-	"UK": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs/_delete_by_query",
-	"CA": "https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/jobs_ca/_delete_by_query",
+func PushToElastic(job LegacyUploadJob, countryCode string, token string) (*http.Response, error) {
+	if countryCode == "UK" || countryCode == "CA" {
+		job.State = ""
+	}
+	jsonData, err := json.Marshal(job)
+	if err != nil {
+		return nil, err
+	}
+	payload := strings.NewReader(string(jsonData))
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://76b76771cccc46ca8ef47110d5142527.eu-west-1.aws.found.io:9243/%s/_doc/%s", elasticDBs[countryCode], job.ID.Hex()), payload)
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("authorization", fmt.Sprintf("Basic %s", token))
+	return http.DefaultClient.Do(req)
+}
+
+var elasticDBs = map[string]string{
+	"US": "jobs_us",
+	"UK": "jobs",
+	"CA": "jobs_ca",
 }
