541e478967c57ddb8b1ae693b08e8e05097b704b
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-06-25 10:46:23 +0100

    streamlined helpers

--- go.mod
+++ go.mod
@@ -1,15 +1,3 @@
 module github.com/joincounter/directlyapply-feed-ingester-helpers
 
 go 1.16
-
-require (
-	github.com/aws/aws-sdk-go v1.38.64 // indirect
-	github.com/gomodule/redigo v1.8.5
-	github.com/google/uuid v1.2.0
-	github.com/klauspost/compress v1.13.1 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
-	go.mongodb.org/mongo-driver v1.5.3
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/text v0.3.6
-)
--- standardJob.go
+++ standardJob.go
@@ -1,11 +1,7 @@
 package helpers
 
 import (
-	"fmt"
-	"math"
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // StandardJob generic job to be used accross the system
@@ -32,75 +28,4 @@ type StandardJob struct {
 	Category       string    `json:"category" bson:"category"`
 	Expires        time.Time `json:"expires" bson:"expires"`
 	Summaries      []string  `json:"summaries" bson:"summaries"`
-}
-
-// EstimatedValue standard calc for the jobs value
-func (sj *StandardJob) EstimatedValue() float64 {
-	CPAEstimatedValue := sj.CPA * 0.7
-	CPCEstimatedValue := sj.CPC * 35
-	return math.Max(float64(CPAEstimatedValue), float64(CPCEstimatedValue))
-}
-
-type LegacyUploadJob struct {
-	Title        string             `bson:"title" json:"title"`
-	Company      string             `bson:"company" json:"company"`
-	Location     string             `bson:"location" json:"location"`
-	City         string             `bson:"city" json:"city"`
-	ZIP          string             `bson:"zip" json:"zip"`
-	State        string             `bson:"state" json:"state"`
-	SourceID     string             `bson:"sourceid" json:"locatsourceidion"`
-	ID           primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
-	Description  string             `bson:"description" json:"description"`
-	Summary      string             `bson:"summary" json:"summary"`
-	Posted       time.Time          `bson:"posted" json:"posted"`
-	Expires      time.Time          `bson:"expires" json:"expires"`
-	EasyApply    bool               `bson:"easyapply" json:"easyapply"`
-	SourceURL    string             `bson:"sourceurl" json:"sourceurl"`
-	SecondaryURL string             `bson:"secondaryurl" json:"secondaryurl"`
-	Slug         string             `bson:"slug" json:"slug"`
-	GA           string             `bson:"ga" json:"ga"`
-	BadLink      bool               `bson:"badlink" json:"badlink"`
-	Logo         string             `bson:"logo" json:"logo"`
-	CPC          float64            `bson:"cpc" json:"cpc"`
-	CPA          float64            `bson:"cpa" json:"cpa"`
-	SalaryMin    float64            `bson:"salaryMinimum" json:"salaryMinimum"`
-	SalaryMax    float64            `bson:"salaryMaximum" json:"salaryMaximum"`
-	SalaryType   string             `bson:"salaryType" json:"salaryType"`
-}
-
-func ConvertToLegacyUploadJob(jobRaw StandardJob) LegacyUploadJob {
-	job := LegacyUploadJob{}
-
-	job.Title = jobRaw.Title
-	job.Description = jobRaw.Description
-
-	job.Company = jobRaw.Company
-	job.Slug = GenerateSlug(job.Company)
-	job.Location = jobRaw.Location
-	job.City = jobRaw.City
-	job.ZIP = jobRaw.ZIP
-	job.CPC = float64(jobRaw.CPC)
-	job.CPA = float64(jobRaw.CPA)
-
-	job.SalaryMin = jobRaw.SalaryMin
-	job.SalaryMax = jobRaw.SalaryMax
-	job.SalaryType = jobRaw.SalaryType
-
-	job.Posted = jobRaw.Date
-	job.Expires = jobRaw.Date.AddDate(0, 0, 7*4)
-
-	job.Summary = formatListOfStringsAsSummary(jobRaw.Summaries)
-
-	job.SourceURL = jobRaw.URL
-	job.SecondaryURL = jobRaw.URL
-
-	return job
-}
-
-func formatListOfStringsAsSummary(strs []string) string {
-	innerhtml := ""
-	for i := 0; i < len(strs); i++ {
-		innerhtml = innerhtml + fmt.Sprintf("<li>%s</li>", strs[i])
-	}
-	return fmt.Sprintf("<ul>%s</ul>", innerhtml)
 }
